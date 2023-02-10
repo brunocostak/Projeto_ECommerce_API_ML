e99dd4815edbd518e2563dbe7e6ab78d42a25043
bruno <bruno.costa1203@yahoo.com>
2023-02-10 08:01:07 -0300

    Whatever

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Categories from '../components/Categories';
+import Products from '../components/Products';
 import * as api from '../services/api';
 
 class Home extends React.Component {
   state = {
-    list: [],
     categories: [],
+    products: [],
+    product: '',
   };
 
   componentDidMount() {
+    this.getCategories();
+  }
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({
+      product: value,
+    });
+  };
+
+  getCategories = async () => {
     api.getCategories().then((categories) => {
       this.setState({ categories });
     });
+  };
 
-    api.getProductsFromCategoryAndQuery('Roda').then((products) => {
-      this.setState({ list: products });
+  getCategory = async (id) => {
+    const { results } = await api.getProductsFromCategoryAndQuery(id, null);
+    this.setState({
+      products: results,
     });
-    console.log(this.state.list);
-  }
+  };
+
+  handleClick = async (categories, product) => {
+    const result = await api.getProductsFromCategoryAndQuery(null, product);
+    this.setState({
+      products: result,
+    });
+  };
 
   render() {
-    const { list, categories } = this.state;
+    const { product, products, categories } = this.state;
     return (
       <>
         <div>
           <Categories
             categories={ categories }
+            onClickCat={ this.getCategory }
           />
           ;
         </div>
@@ -34,12 +56,31 @@ class Home extends React.Component {
           <Link data-testid="shopping-cart-button" to="/cart">Carrinho</Link>
         </div>
         <div data-testid="home-initial-message">
-          {list.length === 0
+          {product.length === 0
             ? (<p>Digite algum termo de pesquisa ou escolha uma categoria.</p>) : null}
           <input
+            data-testid="query-input"
             type="text"
             placeholder="Pesquisar"
+            onChange={ this.handleChange }
           />
+          <button
+            data-testid="query-button"
+            onClick={ () => this.handleClick(categories, product) }
+          >
+            TESTE
+          </button>
+          {
+            products
+              .length === 0 ? (
+                <p>Nenhum produto foi encontrado</p>
+              ) : (
+                <Products
+                  products={ products }
+                />
+              )
+          }
+          <div />
         </div>
       </>
     );
